4c4882a78f5d3ea97129144fb4744a7591c6915f
quanph1501 <quanph1501@gmail.com>
2024-07-10 19:57:42 +0700

    feat: Add NGÀY TRẢI NGHIỆM HOẠT ĐỘNG CÁC CÂU LẠC BỘ - DỰ ÁN page

--- src/store/InitialVariable.tsx
+++ src/store/InitialVariable.tsx
@@ -70,21 +70,30 @@ const InitialVariable = () => {
             bq: "Website có sử dụng một số hình ảnh từ Câu lạc bộ Nhiếp ảnh - Specture và Câu lạc bộ Báo chí - Truyền thông",
             link: "hots/hpd",
             // linkFB: "https://www.facebook.com/759662799498945"
-        }
-        , {
+        }, 
+        {
             img: "/activities/1.jpg",
             title: "Lễ hội âm nhạc",
             subtitle: "",
             bq: "Website có sử dụng một số hình ảnh từ Câu lạc bộ Nhiếp ảnh - Specture và Câu lạc bộ Báo chí - Truyền thông",
             link: "hots/lhan",
             // linkFB: "https://www.facebook.com/541613061448202"
-        }, {
+        }, 
+        {
             img: "/activities/2.jpg",
             title: "Lễ tri ân /và trưởng thành",
             subtitle: "",
             bq: "Website có sử dụng một số hình ảnh từ Dự án khối 12, Câu lạc bộ Nhiếp ảnh - Specture và Câu lạc bộ Báo chí - Truyền thông",
             link: "hots/lta&th",
             // linkFB: "https://www.facebook.com/576520341290807"
+        },
+        {
+            img: "/activities/4.jpg",
+            title: "NGÀY TRẢI NGHIỆM HOẠT ĐỘNG CÁC CÂU LẠC BỘ - DỰ ÁN",
+            subtitle: "",
+            bq: "Website có sử dụng một số hình ảnh từ Câu lạc bộ Nhiếp ảnh - Specture và Câu lạc bộ Báo chí - Truyền thông",
+            link: "hots/ntnhdcclb&da",
+            // linkFB: "https://www.facebook.com/759662799498945"
         },
     ]
 
